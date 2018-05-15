9aa7bd4405c113f0d9e54e86e157c67f5ff7fffc
Aleksander Stanchev <alex_stan@abv.bg>
2018-05-15 17:03:58 +0300

    Update transport_price.js

--- transport_price.js
+++ transport_price.js
@@ -2,17 +2,12 @@ function transportPrice([arg1, arg2]) {
 
     let kilometre = Number(arg1);
     let time = arg2;
-
     let taxiTax = 0.7;
     let taxiDay = 0.79;
     let taxiNight = 0.9;
-
     let busTarif = 0.09;
-
     let trainTarif = 0.06;
-
     let price;
-
 
     if (kilometre>=1 && kilometre<20 && time=="day") {
         price = (kilometre * taxiDay) + taxiTax;
